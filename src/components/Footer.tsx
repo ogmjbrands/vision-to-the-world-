155427148551e6a54e0887774070cccc49ba4eb6
ogmjbrands <ogmjbrandingagency@gmail.com>
2025-09-01 20:43:45 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,24 +10,6 @@ export default function Footer() {
           <span className="font-bold text-xl ml-2">Vision To The World</span>
         </div>
         <p className="text-gray-300 mb-6">Making global travel easy, affordable, and unforgettable.</p>
-
-        <div className="border-t border-gray-800 pt-6 text-xs text-gray-500">
-          <div className="flex flex-wrap justify-center gap-4">
-            <a href="https://www.youtube.com/about/" target="_blank" className="hover:underline">关于</a>
-            <a href="https://www.youtube.com/newseroom/" target="_blank" className="hover:underline">新闻</a>
-            <a href="https://www.youtube.com/about/copyright/" target="_blank" className="hover:underline">版权</a>
-            <a href="https://www.youtube.com/contact/" target="_blank" className="hover:underline">联系我们</a>
-            <a href="https://www.youtube.com/creators/" target="_blank" className="hover:underline">创作者</a>
-            <a href="https://www.youtube.com/advertise/" target="_blank" className="hover:underline">广告</a>
-            <a href="https://developers.google.com/youtube" target="_blank" className="hover:underline">开发者</a>
-            <a href="https://www.youtube.com/about/policies/" target="_blank" className="hover:underline">条款</a>
-            <a href="https://policies.google.com/privacy" target="_blank" className="hover:underline">隐私权</a>
-            <a href="https://www.youtube.com/yt/policyandsafety/" target="_blank" className="hover:underline">政策与安全</a>
-            <a href="https://www.youtube.com/howyoutubeworks/" target="_blank" className="hover:underline">YouTube 的运作方式</a>
-            <a href="https://www.youtube.com/new" target="_blank" className="hover:underline">测试新功能</a>
-          </div>
-          <p className="mt-4">&copy; 2025 Google LLC</p>
-        </div>
       </div>
     </footer>
   );
